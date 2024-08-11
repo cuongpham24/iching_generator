12095c27f4b8836893da56b36ea3a19fdbe3db91
Vinson Pham <cuongpham.iec@gmail.com>
2024-08-11 15:36:33 -0500

    reorganize code

---
+++ ichinglunarcalendar/__init__.py
@@ -0,0 +1,7 @@
+from .converter import Solar, Lunar, DateNotExist, Converter, SexagenaryBasic, Sexagenary
+
+__all__ = [
+    "Solar", "Lunar", "DateNotExist", "Converter", "SexagenaryBasic", "Sexagenary"
+]
+
+__version__ = '0.0.1'
---
+++ ichinglunarcalendar/_constant.py
@@ -0,0 +1,16 @@
+heavenlyStems = ["giáp", "ất", "bính", "đinh", "mậu", "kỷ", "canh", "tân", "nhâm", "quý"]
+heavenlyStemsElement = ["mộc", "mộc", "hỏa", "hỏa", "thổ", "thổ", "kim", "kim", "thủy", "thủy"]
+
+earthlyBranches = ["tý", "sửu", "dần", "mão", "thìn", "tỵ", "ngọ", "mùi", "thân", "dậu", "tuất", "hợi"]
+earthlyBranchesElement = ["thủy", "thổ", "mộc", "mộc", "thổ", "hỏa", "hỏa", "thổ", "kim", "kim", "thổ", "thủy"]
+
+numHeaven = len(heavenlyStems)
+numEarth = len(earthlyBranches)
+
+elements = {
+    "kim": 1,
+    "thủy": 2,
+    "mộc": 3,
+    "hỏa": 4,
+    "thổ": 5
+}
